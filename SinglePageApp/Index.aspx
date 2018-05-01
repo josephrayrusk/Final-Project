78803e1f617bff49c3988c7cf6072b23526b4f88
josephrayrusk <35713802+josephrayrusk@users.noreply.github.com>
2018-04-30 20:34:27 -0700

    Final Project

---
+++ SinglePageApp/Index.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Secondary" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Secondary</title>
+    <script src="Scripts/knockout-3.4.2.js"></script>
+    <script src="Scripts/jQuery.tmpl.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+    <form id="form1" runat="server">
+
+    <div>
+        <h1>Coffee Brewing Calculator</h1>
+        Please select how many cups of coffee.  <select data-bind="options: Cups, optionsText: 'CupNumber', optionsValue:'HowMany', value: selectedCups "></select>
+        <br />
+        Please select the cup size.  <select data-bind="options: Size, optionsText: 'CupSize', optionsValue:'Weight', value: selectedSize "></select>
+        <br />
+        Please select how strong you would like the coffee.  <select data-bind="options: Strength, optionsText: 'CoffeeStrength', optionsValue:'Ratio', value: selectedStrength "></select>
+        <br />
+        <br />
+
+        Using a kitchen scale that measures in grams, please weigh out <strong><span data-bind="text: CoffeeWeight"></span> grams</strong> of coffee.
+        <br />
+        Grind the coffee and brew using your usual method. 
+        <br />
+        <br />
+
+        After it has finished brewing, how Does it Taste?
+        <br />
+        <select data-bind="options: Taste, optionsText: 'CoffeeFlavor', optionsCaption: 'Please Choose...', optionsValue:'Flavor', value: selectedFlavor "></select>
+        <br />
+        <br />
+        <span data-bind="text: selectedFlavor"></span>
+
+   
+    </div>
+
+
+    </form>
+    <script src="Scripts/cupSize.js"></script>
+
+</body>
+</html>
---
+++ SinglePageApp/Scripts/cupSize.js
@@ -0,0 +1,82 @@
+﻿var myViewModel = function () {
+    var self = this;
+    this.Cups = ko.observableArray([
+        {
+            HowMany: 1,
+            CupNumber: 'One Cup',       
+        },
+        {
+            HowMany: 2,
+            CupNumber: 'Two Cups',
+        },
+        {
+            HowMany: 3,
+            CupNumber: 'Three Cups',
+        },
+        {
+            HowMany: 4,
+            CupNumber: 'Four Cups',
+        },
+        ]);
+    this.selectedCups = ko.observable({});
+
+    this.Size = ko.observableArray([
+        {
+            Weight: 226,
+            CupSize: '8oz',
+        },
+        {
+            Weight: 283,
+            CupSize: '10oz',
+        },
+        {
+            Weight: 340,
+            CupSize: '12oz',
+        },
+        {
+            Weight: 453,
+            CupSize: '16oz',
+        },
+    ]);
+    this.selectedSize = ko.observable({}); 
+
+    this.Strength = ko.observableArray([
+        {
+            Ratio: 17,
+            CoffeeStrength: 'Light',
+        },
+        {
+            Ratio: 15,
+            CoffeeStrength: 'Medium',
+        },
+        {
+            Ratio: 13,
+            CoffeeStrength: 'Strong',
+        },
+    ]);
+    this.selectedStrength = ko.observable({});
+
+
+    this.Taste = ko.observableArray([
+        {
+            Flavor: 'Grind the coffee a little more course',
+            CoffeeFlavor: 'Acidic or Overpowering',
+        },
+        {
+            Flavor: 'Congratulations, you nailed it!',
+            CoffeeFlavor: 'Tastes Delicious',
+        },
+        {
+            Flavor: 'Grind the coffee a little more Fine',
+            CoffeeFlavor: 'Bitter or Weak',
+        },
+    ]);
+    this.selectedFlavor = ko.observable({});
+    this.CoffeeWeight = ko.computed(function () {
+
+        return Math.round((this.selectedCups() * this.selectedSize()) / this.selectedStrength());
+    }, this);
+
+}
+
+ko.applyBindings(new myViewModel());
